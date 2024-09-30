750abb672d4d6db02394ad9682653437c3f0c0b1
lihasoo <mansannamnas@gmail.com>
2024-09-30 17:10:48 +0500

    Create testt.py
    v1.0

---
+++ testt.py
@@ -0,0 +1,15 @@
+import wave
+
+audio = wave.open("converted.wav", "rb")
+channels = audio.getnchannels()
+sample_width = audio.getsampwidth()
+frequency = audio.getframerate()
+num_of_frames = audio.getnframes()
+
+print("Channels:", channels)
+print("Sample:", sample_width, "Bytes")
+print("Frequency:", frequency)
+print("Number:", num_of_frames)
+
+samples = audio.readframes(num_of_frames)
+print(len(samples))
